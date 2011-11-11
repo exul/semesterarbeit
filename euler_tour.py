ba7270b812ad47327a77d8d9364ca8dcecbc212a
Andreas Broennimann <andreas.broennimann@v-net.ch>
2011-11-11 14:18:19 +0100

    Create class Euler_Tour
    --HG--
    rename : hierholzer.py => euler_tour.py

---
+++ euler_tour.py
@@ -0,0 +1,126 @@
+import random
+
+from node import Node
+from edge import Edge
+from graph import Graph
+
+class Euler_Tour:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def calculate(self):
+        euler = list()
+        euler_graph = Graph()
+
+        # save start node, so we can check if we did a cycle
+        # we just take the first node in the graph, it doesn't matter
+        start_node = self.graph.nodes[0]
+        # for the first walk our start_node is the current_node
+        current_node = start_node
+        # we want wo walk at least once
+        condition = True
+
+        # get size of the current graph
+        graph_size = self.graph.size
+
+        # TODO: list only for debugging
+        euler_ori = list()
+
+        while euler_graph.size < graph_size:
+            # list to store nodes for each cycle
+            euler_sub = list() 
+            # list to store a copy of the original euler list, needed to merge euler
+            # with euler_sub
+            euler_tmp = list()
+            # loop until we hit the start_node, but at least once
+            while condition:
+                # add the current node to the euler tour
+                euler_sub.append(current_node)
+                # take an arbitrary neighbour
+                neighbours = self.graph.neighbour_nodes(current_node)
+                next_node = random.choice(list(neighbours.copy().keys()))
+                # get the edge between the current node an our new node
+                # if there is more than one edge, just take the first one
+                edge_list = self.graph.edge_by_nodes(current_node, next_node)
+                current_edge = edge_list[0]
+                # delete the edge from the graph, we don't want to the the same edge twice
+                self.graph.remove_edge(current_edge)
+
+                # add nodes and edges to the graph that represents the euler cycle
+                if not euler_graph.contains_node(current_node):
+                    euler_graph.add_node(current_node)
+
+                if not euler_graph.contains_node(next_node):
+                    euler_graph.add_node(next_node)
+
+                current_node.visits += 1
+                euler_graph.add_edge(current_edge)
+
+                # start agein with the next_node as current_node
+                current_node = next_node
+                # check if we hit the start_node
+                condition = (current_node != start_node)
+
+            # edge the last node
+            euler_sub.append(current_node)
+            # merge current cycle with the already computed one
+            euler = self.merge_cycles(euler, euler_sub, current_node) 
+
+            for node in euler:
+                #TODO: Find better solution to get the next_node, best would be a list
+                #with nodes that are candiates (nodes that are in the last cycle and
+                #have at least 1 neighbour)
+                if self.graph.contains_node(node):
+                    neighbours = self.graph.neighbour_nodes(node)
+                    if len(neighbours) > 0:
+                        start_node = node
+                        current_node = start_node
+                        condition = True
+                        break
+
+        print('========== Debug ========')
+        print('Real euler')
+        for node in euler:
+            print('Node {0} visited {1}'.format(node.label, node.visits))
+
+        # set reference of the euler_graph to graph, so we can work on with it
+        graph = euler_graph
+
+        # return a list of nodes in the order of the euler tour
+        return euler
+
+    def merge_cycles(self, euler, euler_sub, current_node):
+        '''
+        Merge two eulerian cycles.
+
+        @type:  euler:list
+        @param: List that contains the eulerian path.
+        @type:  euler_sub:list
+        @param: List that should be merged into eulerian path.
+        @type:  current_node: node
+        @param: Node at which the two lists should be murged
+
+        @rtype: list
+        @return: A list containing all nodes.
+        '''
+        # copy content of euler list to euler_tmp
+        euler_tmp = list()
+        euler_tmp += euler
+        # empty euler list
+        euler = list()
+        # only append list once
+        found = False
+
+        if len(euler_tmp) > 0:
+            for node in euler_tmp:
+                if node == current_node and found == False:
+                    for node_sub in euler_sub:
+                        euler.append(node_sub)
+                    found = True
+                else:
+                    euler.append(node)
+        else:
+            euler += euler_sub
+
+        return euler
+
