072c4645a793e9d8c0424dafbb6e448e9668ebb3
Andreas Broennimann <andreas.broennimann@v-net.ch>
2011-12-04 17:22:46 +0100

    Show steps during calculation

--- solve.py
+++ solve.py
@@ -10,21 +10,46 @@ reader = Reader()
 writer = Writer()
 
 # read graph data from file
-graph = reader.euler_2d('data/eil101.tsp')
+#graph = reader.euler_2d('data/eil101.tsp')
+#graph = reader.euler_2d('data/eil51.tsp')
+#graph = reader.euler_2d('data/a280.tsp')
+#graph = reader.euler_2d('data/ch130.tsp')
+#graph = reader.euler_2d('data/fl1400.tsp')
+#graph = reader.euler_2d('data/kroA100.tsp')
+#graph = reader.euler_2d('data/ulysses22.tsp')
+#graph = reader.euler_2d('data/bier127.tsp')
+#graph = reader.euler_2d('data/bayg29.tsp')
+#graph = reader.euler_2d('data/float.tsp')
+#graph = reader.euler_2d('data/int.tsp')
+graph = reader.euler_2d('data/graph.tsp')
+
+print('Graph is created')
+
+# TODO: write graph as matrix, not needed for the algortihm
+writer.write_matrix(graph, '/tmp/matrix_graph.tsp')
 
 # calculate the minimum spanning tree
 mst = Minimum_Spanning_Tree()
 graph = mst.calculate(graph)
+
+print('MST calculated')
 
 # do a minimum perfect matching on the odd nodes of the graph
 # and add the new edges to the graph
 matcher = Matcher()
 graph = matcher.calculate(graph)
 
+print('Perfect Matching calculated')
+
 euler_tour = Euler_Tour()
 euler_nodes = euler_tour.calculate(graph)
 
+print('Euler Tour calculated')
+
 tsp_nodes = euler_tour.shorten(euler_nodes)
+
+print('Euler Tour shortened')
+
 writer.write_nodes(tsp_nodes, '/tmp/tour.tsp')
 
 tsp_cost = 0
