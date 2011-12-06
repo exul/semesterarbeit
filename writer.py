a7f268b62e60fe4402933aa1c899dd612aa61b89
Andreas Broennimann <andreas.broennimann@v-net.ch>
2011-12-06 16:35:13 +0100

    New function to write all edges of a graph into a file

--- writer.py
+++ writer.py
@@ -4,33 +4,33 @@ class Writer:
     '''
     Write a graph or a list of nodes into a file.
     '''
-    #def write_blossom_iv(self, graph, file_location):
-        #'''
-        #Write a graph to a file, use the blossom IV format
-
-        #@type  graph: graph
-        #@param graph: graph that should be written to file
-
-        #@type  file_location: string
-        #@param     location to save the file
-        #'''
-        #f = open(file_location, 'w')
-        
-        ## write number of nodes and edges
-        #print('{0} {1}'.format(graph.size, graph.edge_count), file=f)
-        
-        ## write and edge on every line
-        ## ID node_1 node_2 weight
-        ##TODO: Use a more generic solution, do not just print odd_node_nr
-        #for node in graph.nodes:
-            #for neighbour in graph.neighbour_nodes(node):
-                #edge_list = graph.edge_by_nodes(node, neighbour)
-                #for edge in edge_list:
-                    #print('{0} {1} {2}' \
-                            #.format(node.odd_node_nr, neighbour.odd_node_nr,
-                                #edge.weight), \
-                                        #file=f)
-        #f.close()
+#    def write_blossom_iv(self, graph, file_location):
+#        '''
+#        Write a graph to a file, use the blossom IV format
+#
+#        @type  graph: graph
+#        @param graph: graph that should be written to file
+#
+#        @type  file_location: string
+#        @param     location to save the file
+#        '''
+#        f = open(file_location, 'w')
+#        
+#        # write number of nodes and edges
+#        print('{0} {1}'.format(graph.size, graph.edge_count), file=f)
+#        
+#        # write and edge on every line
+#        # ID node_1 node_2 weight
+#        #TODO: Use a more generic solution, do not just print odd_node_nr
+#        for node in graph.nodes:
+#            for neighbour in graph.neighbour_nodes(node):
+#                edge_list = graph.edge_by_nodes(node, neighbour)
+#                for edge in edge_list:
+#                    print('{0} {1} {2}' \
+#                            .format(node.odd_node_nr, neighbour.odd_node_nr,
+#                                edge.weight), \
+#                                        file=f)
+#        f.close()
 
     def write_nodes(self, nodes, file_location):
         '''
@@ -48,6 +48,25 @@ class Writer:
             print('{0} {1} {2}'.format(node.nr, node.x, node.y), file=f)
 
         f.close()
+
+    def write_edges(self, graph, file_location):
+        '''
+        Write all edges of a graph into a file. The file format is 
+        node_1 node_2 weight, so each line contains one edge.
+
+        @type   graph: graph
+        @param  graph: graph that contains the edges
+
+        @type   file_location: string
+        @param  file_location: location to save the file
+        '''
+        f = open(file_location, 'w')
+        edges = graph.edges
+
+        for idx, edge in enumerate(edges):
+            print('{0} {1} {2}' \
+                .format(edge.node_1.nr, edge.node_2.nr, edge.weight), file=f)
+
 
     def write_matrix(self, graph, file_location):
         '''
